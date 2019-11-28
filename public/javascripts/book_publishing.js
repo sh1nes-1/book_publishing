0ef180d861a347da71055f7cf04c4d38186ddba3
Sh1ne <kosovchychserhiy@gmail.com>
2019-11-28 17:44:36 +0200

    Add persons page

--- public/javascripts/book_publishing.js
+++ public/javascripts/book_publishing.js
@@ -1,9 +1,24 @@
-var app = angular.module('book_publishing', ['ngRoute'])
+var app = angular.module('book_publishing', ['ngResource','ngRoute'])
 
 app.config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/', {
-        templateUrl: 'partials/home.html'
-    }).otherwise({
-        redirectTo: '/'
-    });
+    $routeProvider
+        .when('/', {
+            templateUrl: 'partials/home.html'
+        })
+        .when('/persons', {
+            templateUrl: 'partials/persons.html',
+            controller: 'PersonsCtrl'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
 }]);
+
+app.controller('PersonsCtrl', ['$scope', '$resource', 
+    function($scope, $resource) {
+        var Persons = $resource('/api/persons');
+        Persons.query(function(persons) {
+            $scope.persons = persons;
+        });
+    }
+]);
